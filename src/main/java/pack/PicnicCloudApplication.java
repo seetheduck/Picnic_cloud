12dc78c4f75334685230a5657566b84db6ec2fbc
seetheduck <lwj0506@naver.com>
2024-08-14 18:53:45 +0900

    작업 준비

---
+++ src/main/java/pack/PicnicCloudApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicnicCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicnicCloudApplication.class, args);
+	}
+
+}
