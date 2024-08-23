e4a90dedba57c823936a7ee63f236beac1811e3b
caethy08 <caethy@naver.com>
2024-08-23 11:24:33 +0900

    메인페이지 cdn파일 작업

---
+++ vue/mainPage_Vue/src/components/TopBar.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+const TopBar = {
+    template: `
+      <header>
+        <div id="upper-nav">
+          <div id="logo"><router-link to="/">Picnic_Cloud</router-link></div>
+          <nav>
+            <ul>
+              <li v-if="!isLoggedIn"><router-link to="/login">login</router-link></li>
+              <li v-if="isLoggedIn">
+                <router-link to="/mypage">MyPage</router-link>
+                <span @click="goToChat" class="notification-icon">🔔</span>
+              </li>
+            </ul>
+          </nav>  
+        </div>
+        <nav class="bottom-nav">
+          <ul>
+            <li><router-link to="" >테마 추천</router-link></li>
+            <li><router-link to="/market">중고거래</router-link></li>
+            <li><router-link to="/map">지도</router-link></li>
+            <li><router-link to="/calendar">행사일정</router-link></li>
+            <li><router-link to="/book">이달의 책</router-link></li>
+          </ul>
+        </nav>
+      </header>
+    `,
+    data() {
+      return {
+        isLoggedIn: false, // 임시로 false로 설정. 실제 값은 로그인 로직에 따라 변경되어야 함
+      };
+    },
+    methods: {
+      goToChat() {
+        alert('Go to chat!'); // 실제로는 라우팅 또는 다른 로직이 필요
+      }
+    }
+  };
+  
