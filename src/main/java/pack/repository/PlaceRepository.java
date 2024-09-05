1106c44cc902fb4a14703482a9e99a8adc71ddb0
yoonah0327 <yoonah6719@gmail.com>
2024-09-05 12:15:32 +0900

    장소정보출력

--- src/main/java/pack/repository/PlaceRepository.java
+++ src/main/java/pack/repository/PlaceRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -13,17 +14,9 @@ import org.springframework.data.repository.query.Param;
 import pack.entity.PlaceEntity;
 
 public interface PlaceRepository extends JpaRepository<PlaceEntity, Integer>, JpaSpecificationExecutor<PlaceEntity> {
-	
-	//테마별 장소목록. 검색어 있을경우 검색어 필터링 결과값으로.
-	//jpa기본제공메소드 사용하기에 생략.
-//select p from PlaceEntity as p where p.placeType = :placeType
-//	+ "and (:keyword is null or :keyword = '' " 
-//	+ "or p.name like concat('%', :keyword, '%') " 
-//	+ "or p.address like concat('%', :keyword, '%')) "
-//	+ "order by p.point desc, p.likeCnt desc")
-	
-	//장소 페이징처리
-	Page<PlaceEntity> findByPlaceType(String placeType, Pageable pageable);
+
+	//타입별 장소 목록 출력. 검색있을시 필터링(동적쿼리). 페이징처리
+	Page<PlaceEntity> findAll(Specification<PlaceEntity> spec, Pageable pageable);
 	
 	//선택한 장소 1곳 상세정보. 
 	Optional<PlaceEntity> findByNo(int no);
