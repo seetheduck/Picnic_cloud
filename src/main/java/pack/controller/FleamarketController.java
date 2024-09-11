8f265286d0caa9f5c7b2b32227e32e7785afe615
hi-haho <from_me_you@naver.com>
2024-09-11 10:01:35 +0900

    page sort 변경

--- src/main/java/pack/controller/FleamarketController.java
+++ src/main/java/pack/controller/FleamarketController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -42,7 +43,7 @@ public class FleamarketController {
 			@RequestParam(value = "search", required = false) String search,
 			HttpServletRequest request
 	) {
-		Pageable pageable = PageRequest.of(page, size);
+		Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdate"));
 
 		// 현재 사용자 ID 가져오기 (비로그인 사용자의 경우 null 처리)
 		String userId = null;
