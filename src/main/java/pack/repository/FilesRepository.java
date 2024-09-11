be3137fc0f846c7ca2857068ae0e5e62d08ca4a2
hi-haho <from_me_you@naver.com>
2024-09-11 09:33:03 +0900

    flea로직 보완

--- src/main/java/pack/repository/FilesRepository.java
+++ src/main/java/pack/repository/FilesRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 
 import pack.entity.FilesEntity;
 
+import java.util.List;
+
 
 public interface FilesRepository extends JpaRepository<FilesEntity,Integer> {
 
 	//추가시 증가용) 파일 가장 큰 번호
 	 @Query("select Max(f.no) from FilesEntity f")
 	 Integer findbyMaxNo();
-	
+
+	// 특정 플리마켓 게시물 번호에 해당하는 파일 정보 가져오기
+	List<FilesEntity> findByFleamarketEntity_No(Integer fleaMarketNo);
 }
