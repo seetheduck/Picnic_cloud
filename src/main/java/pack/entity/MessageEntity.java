a6f77b87373864e5edcfd1f7b5625d70e6ac7925
seetheduck <lwj0506@naver.com>
2024-09-19 16:59:49 +0900

    MessageEntity 변경

--- src/main/java/pack/entity/MessageEntity.java
+++ src/main/java/pack/entity/MessageEntity.java
@@ -2,12 +2,7 @@ package pack.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,7 +21,11 @@ public class MessageEntity {
     @Id
     private Integer no;
     private String senderId;
-    private Boolean readIs;
+
+    @Column(name = "read_is", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
+    @Builder.Default
+    private Boolean readIs = false;;
+
     private String messageContents;
     private LocalDateTime createDate;
 
